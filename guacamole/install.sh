c0a8d94f0e1e21a65e785aad54952e2dd5cebc79
Sergio López <sergio.lophug@gmail.com>
2021-09-14 16:41:44 +0200

    Guacamole

---
+++ guacamole/install.sh
@@ -0,0 +1,24 @@
+# Create namespace
+
+kubectl apply -f guacamole-namespace.yaml
+
+# Create PVC for Post -> guacamole-postgresql
+kubectl apply -f postgresql-pvc.yaml
+
+# Deploy postgresql
+#   https://github.com/bitnami/charts/tree/master/bitnami/postgresql
+#   
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install --name postgresql --namespace guacamole  -f postgresql-values.yaml  bitnami/postgresql
+
+# Deploy Guacamole
+#   https://artifacthub.io/packages/helm/halkeye/guacamole
+#   https://github.com/halkeye-helm-charts/guacamole
+
+helm install --name guacamole --namespace guacamole  -f guacamole-values.yaml halkeye/guacamole
+
+
+#https://avi-covid19.datahub.egi.eu:10443/guacamole/#/
+# Create admin user
+
+# https://stackoverflow.com/questions/59862016/apache-guacamole-create-user-using-postgres
