6b10f999dd8a3f3b94542c8f283028cd5ee5025c
Sergio López <sergio.lophug@gmail.com>
2021-10-29 13:19:23 +0200

    kyverno

---
+++ kyverno/install.sh
@@ -0,0 +1,14 @@
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo update
+
+# Beginning with Kyverno 1.4.2, Kyverno Helm chart v2.0.2, the Kyverno CRDs must be added seperately and before Kyverno is installed.
+helm install kyverno-crds kyverno/kyverno-crds --namespace kyverno --create-namespace
+
+helm install kyverno  --namespace kyverno -f kyverno-values.yml kyverno/kyverno
+
+helm repo add policy-reporter https://kyverno.github.io/policy-reporter
+helm repo update
+
+helm install policy-reporter --namespace kyverno -f policyreporter-values.yml  policy-reporter/policy-reporter 
+
+#kubectl port-forward service/policy-reporter-ui 12000:8080 -n kyverno
