5e248343fe1dcad0ac83122d059955389110590d
Sergio López <sergio.lophug@gmail.com>
2022-01-12 11:48:12 +0100

    harbor to new priorityClass

--- harbor/install.sh
+++ harbor/install.sh
@@ -7,7 +7,8 @@ kubectl apply -f pvc-harbor.yaml
 #helm repo add harbor https://helm.goharbor.io
 #helm repo update
 
-git clone -b 1.6.0 https://github.com/goharbor/harbor-helm.git
+#git clone -b 1.6.0 https://github.com/goharbor/harbor-helm.git
+git clone -b 1.8.0 https://github.com/goharbor/harbor-helm.git
 
 #helm install harbor --namespace harbor  -f values.yml harbor/harbor --version 1.6.0 
 helm install harbor --namespace harbor  -f values-persistence.yaml ./harbor-helm
