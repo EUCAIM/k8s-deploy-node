fc6b94f8489f78598a2cb6a10806f13fbddbdb7f
Pablo Montoliu Rico <100042312+PMontoliuRico@users.noreply.github.com>
2026-06-02 08:41:00 +0200

    Add init_node.sh for initializing Orthanc wrapper

---
+++ orthanc/wrapper/init_node.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+npm install pm2@latest -g
+
+cd /node_data
+
+if [ ! -d "bundle" ]; then
+  tar -xzf orthanc-wrapper.tar.gz
+  cd bundle/programs/server
+  npm install
+  cd /node_data
+fi
+
+pm2-runtime app.json
