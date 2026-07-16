93dbe76e472f82150346c1dd76fa319a5d115168
Pablo Montoliu Rico <100042312+PMontoliuRico@users.noreply.github.com>
2026-07-16 12:51:55 +0200

    Fix script indentation and formatting issues

--- orthanc/wrapper/init_node.sh
+++ orthanc/wrapper/init_node.sh
@@ -6,6 +6,9 @@ cd /node_data
 
 if [ ! -d "bundle" ]; then
   tar -xzf orthanc-wrapper.tar.gz
+fi
+
+if [ -d "bundle/programs/server" ]; then
   cd bundle/programs/server
   npm install
   cd /node_data
